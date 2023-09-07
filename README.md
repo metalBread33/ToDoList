1ea19b69e9ee963cde1f7e082b64b80ab4f37ef5
metalBread33 <kfoughty80@gmail.com>
2023-09-07 16:15:41 -0400

    Add menu to main.cpp

---
+++ README.md
@@ -0,0 +1,3 @@
+This app will be a CLI that allows users to create a todo lists. Each todo task can be broken down into further todo lists to keep track of 
+steps that need to be completed for each task
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include "ToDo.h"
+#include <cctype>
 
 int main()
 {
-	std::cout<<"Hello" << std::endl;
+	std::vector<ToDo> ToDoList;
+	char input;
+	bool quit = false;
+	std::cout<<"Welcome to ToDo CLI" << std::endl;
+
+	do
+	{
+		std::cout << "What would you like to do" << std::endl;
+		std::cout << "C - Create\nR - Read\nU - Update\nD - Delete" 
+		  "\nQ - Quit"<< std::endl;
+		std::cin >> input;
+		input = tolower(input);
+		if (input == 'q')
+			quit = true;
+		else if (input == 'c')
+			std::cout << "Create" << std::endl;
+		else if (input == 'r')
+			std::cout << "Read" << std::endl;
+		else if (input == 'u')
+			std::cout << "Update" << std::endl;
+		else if (input == 'd')
+			std::cout << "Delete" << std::endl;
+		else 
+			std::cout << "Invalid. Please try again" << std::endl; 
+	} while (!quit);
+
+
 	return 0;
 }
